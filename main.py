94ae2df991fd69a9e8931ec44eae2a690beb2965
Amber Shifflett <amber.z.shifflett@outlook.com>
2024-09-05 12:30:47 -0700

    Defines the first two methods in GameBoard class

--- main.py
+++ main.py
@@ -13,4 +13,22 @@ class GameSpot:
 
 class GameBoard:
     def __init__(self, fillChar):
-        self.grid = pass
+        self.grid = [[GameSpot() for _ in range(12)] for _ in range(12)]
+        #creates a 12x12 grid initialized with blank GameSpots
+    def getSymbol(self, spot):
+        if spot.occupiedBy == 'untested water':
+            return '?'
+        elif spot.occupiedBy == 'hit':
+            return 'X'
+        elif spot.occupiedBy == 'miss':
+            return 'O'
+        elif spot.occupiedBy == 'water':
+            return 'W'
+        elif spot.occupiedBy == 'ship':
+            return 'S'
+        elif spot.occupiedBy == '':
+            return 'E' # E for unassigned Error
+        else:
+            return 'U' # U for Unrecognized error
+    def printGrid(self):
+        pass
