607c0c93b8b283f6d5160f03853db4b8daa74920
Amber Shifflett <amber.z.shifflett@outlook.com>
2024-09-11 16:48:15 -0700

    Modifies GameSpot and GameBoard
    Expands object constructors. Defines the printGrid method.

--- main.py
+++ main.py
@@ -5,30 +5,48 @@ import random
 
 class GameSpot:
     def __init__(self):
-        self.occupiedBy = ''  #initialized to blank string by default, since only 2/4 boards wil default to water
+        self.occupiedBy = ''
+        #initialized to blank string by default,
+        #since only 2/4 boards wil default to water
+    def __init__(self, fillChar):
+        self.occupiedBy = fillChar
     def setLocation(self, newOccupant):
         self.occupiedBy = newOccupant
     def getLocation(self):
         return self.occupiedBy
-
-class GameBoard:
-    def __init__(self, fillChar):
-        self.grid = [[GameSpot() for _ in range(12)] for _ in range(12)]
-        #creates a 12x12 grid initialized with blank GameSpots
-    def getSymbol(self, spot):
-        if spot.occupiedBy == 'untested water':
+        
+    def getSymbol(self):
+        if self.occupiedBy == 'untested water':
             return '?'
-        elif spot.occupiedBy == 'hit':
+        elif self.occupiedBy == 'hit':
             return 'X'
-        elif spot.occupiedBy == 'miss':
+        elif self.occupiedBy == 'miss':
             return 'O'
-        elif spot.occupiedBy == 'water':
+        elif self.occupiedBy == 'water':
             return 'W'
-        elif spot.occupiedBy == 'ship':
+        elif self.occupiedBy == 'ship':
             return 'S'
-        elif spot.occupiedBy == '':
+        elif self.occupiedBy == '':
             return 'E' # E for unassigned Error
         else:
             return 'U' # U for Unrecognized error
+
+class GameBoard:
+    def __init__(self):
+        self.grid = [[GameSpot() for _ in range(12)] for _ in range(12)]
+        #creates a 12x12 grid initialized with blank GameSpots
+    def __init__(self, fillChar):
+        self.grid = [[GameSpot(fillChar) for _ in range(12)] for _ in range(12)]
+        #creates a 12x12 grid initialized with fillChar GameSpots
     def printGrid(self):
-        pass
+        print("   " + " ".join([chr(i + 65) for i in range(0, 12)]))
+        for i in range(0, 12):
+            row = self.grid[i]
+            returnString = '' + str(i + 1) + ' '
+            if ((i + 1) < 10):
+                returnString += ' '
+            for j in range(0,12):
+                returnString += row[j].getSymbol() + ' '
+            print(returnString)
+
+
