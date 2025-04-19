b9c9695e11e4d89f828f954e637260315dda4271
Amber Shifflett <amber.z.shifflett@outlook.com>
2025-04-19 11:55:05 -0700

    Turns ship insertion into a function and adds is_ship check

--- main.py
+++ main.py
@@ -90,19 +90,35 @@ print('Before we get started, let\'s set up your ships!')
 print('You have 5 ships to place on your friendly grid. Coordinates are in the form of "x y" (x[space]y) where x is the column and y is the row.')
 print('Let\'s start with the first ship.')
 print('The first ship is 3 squares long. Provide the starting coordinates for the ship.')
-try:
-    ship_start = input('Enter coordinates (x y): ')
-    x, y = map(int, ship_start.split())
-    if 0 <= x < user.friendly_grid.size and 0 <= y < user.friendly_grid.size:
-        print(f'Placing ship at ({x}, {y})')
-        # Hold final changes until all ship spots are validated
-        user.friendly_grid.get_area(x, y).symbol = 'S'
-        # user.friendly_grid.set_area(y, x, Area(x, y, is_ship=True, symbol='S'))
-    else:
-        print('Coordinates out of bounds. Please try again.')
-except ValueError:
-    print('Invalid input. Please enter coordinates in the form "x y".')
-print('Your friendly grid after placing the first part of the first ship:')
-user.friendly_grid.display()
-print('"Selected" area details')
-user.friendly_grid.get_area(x, y).debugPrint()
+def insert_ship(player):
+    area_list = []
+    print('Inserting ship...')
+    try:
+        ship_start = input('Enter coordinates (x y): ')
+        x, y = map(int, ship_start.split())
+        if 0 <= x < user.friendly_grid.size and 0 <= y < user.friendly_grid.size:
+            print(f'Placing ship at ({x}, {y})')
+            # Hold final changes until all ship spots are validated
+            # Check if the area is already occupied by another ship
+            if user.friendly_grid.get_area(x, y).is_ship:
+                print('Area already occupied by another ship. Please try again.')
+                return False
+        else:
+            print('Coordinates out of bounds. Please try again.')
+            return False
+    except ValueError:
+        print('Invalid input. Please enter coordinates in the form "x y".')
+        return False
+    print('"Selected" area details')
+    user.friendly_grid.get_area(x, y).debugPrint()
+    print('New area(s) details')
+    area_list.append(Area(x, y, is_ship=True, symbol='S'))
+    for area in area_list:
+        area.debugPrint()
+    # TODO -- Check if the ship has more spots to fill
+    # TODO -- Place the areas in the grid
+    return True
+# Test the insert_ship function
+result = insert_ship(user)
+print(f'Insert ship result: {result}')
+print('End of test zone')
